9fc8251e2ac751d433f4cf0f37ee4629ec1ffff7
Lionel ARNAUD <djspatule@wanadoo.fr>
2017-08-08 19:19:42 +0200

    implemented create, new, edit, update, show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,21 +1,32 @@
 class RecipesController < ApplicationController
   def new
-
+    @recipe = Recipe.new
   end
   def index
     @recipes = Recipe.all
   end
   def create
-
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+           redirect_to recipes_path
+        else
+            render :new
+        end
   end
 
   def edit
-
+    @recipe = Recipe.find(params[:id].to_i)
   end
   def show
-
+    @recipe = Recipe.find(params[:id].to_i)
   end
   def update
+    @recipe = Recipe.find(params[:id].to_i)
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    else
+      render :edit
+    end
 
   end
   def destroy
