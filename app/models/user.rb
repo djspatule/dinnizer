e516759576d9b6cb904df388ec1358cbbc2b51b3
Lionel ARNAUD <djspatule@wanadoo.fr>
2017-08-10 16:06:28 +0000

    implemented user s avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attachment :avatar
   validates :email, presence: true, uniqueness: true
 end
