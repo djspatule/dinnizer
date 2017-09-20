d49708863ffcfc0306d32a2d4a0a97c1d2fc7586
Lionel ARNAUD <djspatule@wanadoo.fr>
2017-09-20 13:31:29 +0000

    improved navigation with buttons in shows, edits and updates and started solving the duplication problem in dinner-update on recipes and guests....need to analyse the server console more closely

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -14,14 +14,15 @@ class DinnersController < ApplicationController
   def create
     @dinner = Dinner.new(dinner_params)
     @dinner.user = current_user
-    if dinner_params[:recipe_id]
-      @dinner_recipe = DinnerRecipe.new(dinner_params)
-      if @dinner_recipe.save
-        redirect_to dinners_path
-      else
-        render :new
-      end
-    end
+    @recipes = Recipe.where(user: current_user)
+    @guests = Guest.where(user: current_user)
+    # why is the following useless ????
+    # if dinner_params[:recipe_id]
+    #   @dinner_recipe = DinnerRecipe.new(dinner_params)
+    #   if @dinner_recipe.save
+    #     redirect_to dinners_path
+    #   end
+    # end
     if @dinner.save
       redirect_to dinners_path, notice: 'Dinner successfully saved !'
     else
@@ -31,6 +32,8 @@ class DinnersController < ApplicationController
 
   def edit
     @dinner = Dinner.find(params[:id].to_i)
+    @recipes = Recipe.where(user: current_user)
+    @guests = Guest.where(user: current_user)
   end
 
   def show
@@ -44,9 +47,44 @@ class DinnersController < ApplicationController
 
   def update
     @dinner = Dinner.find(params[:id].to_i)
+    @dinner_recipes = DinnerRecipe.where(dinner_id: @dinner.id)
+    @dinner_guests = DinnerGuest.where(dinner_id: @dinner.id)
     if @dinner.user == current_user
+    recipe_ids = dinner_params[:dinner_recipes_attributes => [:recipe_id]]
+    puts(recipe_ids)
+      # -- RECIPES -- 
+      # ADD RECIPE TO DINNER : for each recipe that was sent by the user in the "edit form", check if it was already saved or if it is new...and if it is new, save it !
+      if recipe_ids
+        recipe_ids.each do |recipe_id|
+          already_saved = false
+          @dinner_recipes.each do |dinner_recipe|
+            if dinner_recipe.recipe_id == recipe_id
+              already_saved = true
+            end
+            if already_saved == false
+              DinnerRecipe.create!(dinner_recipe.dinner_id, dinner_recipe.recipe_id)
+            end
+          end
+        end
+      end
+      
+      # DELETE RECIPE FROM DINNER : for each recipe that is already saved, check if it was also sent by the user in the "edit form"...and if not, destroy it !
+      @dinner_recipes.each do |dinner_recipe|
+        already_saved = false
+        if recipe_ids
+          recipe_ids.each do |recipe_id|
+            if recipe_id == dinner_recipe.recipe_id
+              already_saved = true
+            end
+          end
+          if already_saved == false
+            dinner_recipe.destroy
+          end
+        end
+      end
+      
       if @dinner.update(dinner_params)
-        redirect_to dinners_path, notice: 'Recipe successfully updated !'
+        redirect_to @dinner, notice: 'Recipe successfully updated !'
       else
         render :edit
       end
@@ -72,8 +110,10 @@ private
 # a good pattern since you'll be able to reuse the same permit
 # list between create and update. Also, you can specialize this method
 # with per-user checking of permissible attributes.
+
+#add :recipe_id, :guest_id ?
   def dinner_params
-    params.require(:dinner).permit(:dinner_date, :recipe_id, :guest_id, :user, :dinner_recipes_attributes => [:recipe_id], :dinner_guests_attributes => [:guest_id])
+    params.require(:dinner).permit(:id, :dinner_date, :user_id, :dinner_recipes_attributes => [:recipe_id], :dinner_guests_attributes => [:guest_id])
   end
 
 
